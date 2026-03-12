f8009a6764d9f506927e67b083919fabf4de85d0
IWAMON007 <tk220062kousei.iwamoto@gmail.com>
2026-03-13 00:39:09 +0900

    formタブで動くように修正

--- main.go
+++ main.go
@@ -7,15 +7,25 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>hello</h1>")
+	h := `<h1>計算できる</h1>`
+	form := `<form action="/cal" method="GET"><input name="length"><input type="submit"></form>`
+
+	fmt.Fprintln(w, h+form)
 }
 
 func calSquareArea(w http.ResponseWriter, r *http.Request) {
 	l := r.URL.Query().Get("length")
-	oneSide, _ := strconv.Atoi(l)
+	oneSide, err := strconv.Atoi(l)
+
+	if err != nil {
+		fmt.Printf("入力された文字列:%s", l)
+		fmt.Println("数値ではない、文字列が入力されました。")
+	}
 
 	area := oneSide * oneSide
-	fmt.Fprintf(w, "<p>One side length : %s Squar = %d</p> ", l, area)
+	p := "<p>One side length : " + l + " Squar = " + strconv.Itoa(area) + "</p> "
+	a := `<a href="/">ここをクリック</a>`
+	fmt.Fprint(w, p+a)
 }
 
 func main() {
