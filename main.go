a0e26175b9928704ea19bd196ab5b8d82f720728
IWAMON007 <tk220062kousei.iwamoto@gmail.com>
2026-03-12 23:08:12 +0900

    簡単な正方形の面積を計算するリクエスト

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>hello</h1>")
 }
 
+func calSquareArea(w http.ResponseWriter, r *http.Request) {
+	l := r.URL.Query().Get("length")
+	oneSide, _ := strconv.Atoi(l)
+
+	area := oneSide * oneSide
+	fmt.Fprintf(w, "<p>One side length : %s Squar = %d</p> ", l, area)
+}
+
 func main() {
 	http.HandleFunc("/", handler)
-	// http.HandleFunc("/cal", calSquareArea)
+	http.HandleFunc("/cal", calSquareArea)
 	http.ListenAndServe(":8080", nil)
 }
