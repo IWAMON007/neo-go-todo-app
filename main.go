99ea42c770503224412a1dbcdc48d7346fe82f6a
IWAMON007 <tk220062kousei.iwamoto@gmail.com>
2026-03-12 23:04:23 +0900

    helloの出力

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>hello</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	// http.HandleFunc("/cal", calSquareArea)
+	http.ListenAndServe(":8080", nil)
+}
