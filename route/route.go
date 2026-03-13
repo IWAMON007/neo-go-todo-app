c9794d5940337c54f33f14b64e18fd850feecef1
IWAMON007 <tk220062kousei.iwamoto@gmail.com>
2026-03-14 00:19:08 +0900

    冗長を削減

--- route/route.go
+++ route/route.go
@@ -1,7 +1,6 @@
 package route
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -14,16 +13,13 @@ type Todo struct {
 var TodoList []Todo
 var nextID int = 1
 
+// htmlを事前にパース
+// hmtlにエラーがあれば、ここで止まる
+var homeHTML = template.Must(template.ParseFiles("home.html"))
+
 // 初期表示時
 func home(w http.ResponseWriter, _ *http.Request) {
-	html, err := template.ParseFiles("home.html")
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	html.Execute(w, nil)
+	homeHTML.Execute(w, TodoList)
 }
 
 // タスクの追加
@@ -33,14 +29,7 @@ func addTask(w http.ResponseWriter, r *http.Request) {
 	TodoList = append(TodoList, Todo{ID: nextID, Task: t})
 	nextID++
 
-	html, err := template.ParseFiles("home.html")
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	html.Execute(w, TodoList)
+	homeHTML.Execute(w, TodoList)
 }
 
 // ルーティング設定
