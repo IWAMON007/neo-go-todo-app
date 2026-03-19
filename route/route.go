3f71010912711a22395e756986caaf90beb6e001
IWAMON007 <tk220062kousei.iwamoto@gmail.com>
2026-03-19 21:52:15 +0900

    タスクの完了機能を実装

--- route/route.go
+++ route/route.go
@@ -1,14 +1,16 @@
 package route
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
 )
 
 type Todo struct {
-	ID   int
-	Task string
+	ID     int
+	Task   string
+	IsDone bool
 }
 
 var TodoList []Todo
@@ -27,8 +29,23 @@ func home(w http.ResponseWriter, _ *http.Request) {
 func addTask(w http.ResponseWriter, r *http.Request) {
 	t := r.URL.Query().Get("task")
 
-	TodoList = append(TodoList, Todo{ID: nextID, Task: t})
+	TodoList = append(TodoList, Todo{ID: nextID, Task: t, IsDone: false})
 	nextID++
+
+	homeHTML.Execute(w, TodoList)
+}
+
+func doneTask(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	taskId, _ := strconv.Atoi(id)
+
+	for i, t := range TodoList {
+		if t.ID == taskId {
+			TodoList[i].IsDone = true
+		}
+	}
+
+	fmt.Println(TodoList)
 
 	homeHTML.Execute(w, TodoList)
 }
@@ -53,6 +70,7 @@ func SetRoute() {
 		"/":            home,
 		"/task":        addTask,
 		"/task/delete": deteleTask,
+		"/task/done":   doneTask,
 	}
 
 	for r, h := range route {
